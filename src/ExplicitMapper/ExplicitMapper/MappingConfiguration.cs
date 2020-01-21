27f908077b9ae26c16506c35ccede45b8bc53bc3
Taras Tyrsa <taras@DESKTOP-1N9956B>
2020-01-21 19:36:13 +0200

    initial commit

---
+++ src/ExplicitMapper/ExplicitMapper/MappingConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ExplicitMapper
+{
+    public class MappingConfiguration
+    {
+        private static readonly List<RawMapping> _rawMappings = new List<RawMapping>();
+
+        internal static IReadOnlyCollection<RawMapping> RawMappings => _rawMappings;
+
+        protected TDest Map<TDest>(object source)
+        {
+            return default(TDest);
+        }
+
+        protected RawMapping<TSource, TDest> CreateMap<TSource, TDest>()
+        {
+            var mapping = new RawMapping<TSource, TDest>();
+            _rawMappings.Add(mapping);
+            return mapping;
+        }
+    }
+}
---
+++ src/ExplicitMapper/ExplicitMapper/RawMapping.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ExplicitMapper
+{
+    public abstract class RawMapping
+    {
+        private readonly List<(Expression source, Expression dest)> _expressions =
+            new List<(Expression source, Expression dest)>();
+
+        internal abstract Type SourceType { get; }
+        internal abstract Type DestinationType { get; }
+        internal IReadOnlyList<(Expression source, Expression dest)> Expressions => _expressions;
+
+        protected void AddExpressionPair(Expression source, Expression dest)
+        {
+            _expressions.Add((source, dest));
+        }
+    }
+
+    public class RawMapping<TSource, TDest> : RawMapping
+    {
+        internal override Type SourceType => typeof(TSource);
+        internal override Type DestinationType => typeof(TDest);
+
+        public RawMapping<TSource, TDest> For<TDestMember>(
+            Expression<Func<TDest, TDestMember>> dest, Expression<Func<TSource, TDestMember>> source)
+        {
+            if (dest.Body.NodeType != ExpressionType.MemberAccess)
+            {
+                throw new ArgumentException(nameof(dest));
+            }
+
+            AddExpressionPair(source, dest);
+
+            return this;
+        }
+    }
+}
