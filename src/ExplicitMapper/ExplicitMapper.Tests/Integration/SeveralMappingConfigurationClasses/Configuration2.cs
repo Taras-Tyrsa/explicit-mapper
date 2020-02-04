292f03853930b0715ef18b34aaec64a3c6a98979
Taras Tyrsa <taras@DESKTOP-1N9956B>
2020-02-04 20:20:39 +0200

    test for using several mapping confguration classes

---
+++ src/ExplicitMapper/ExplicitMapper.Tests/Integration/SeveralMappingConfigurationClasses/Configuration2.cs
@@ -0,0 +1,16 @@
+﻿namespace ExplicitMapper.Tests.Integration.SeveralMappingConfigurationClasses
+{
+    class Configuration2 : MappingConfiguration
+    {
+        public Configuration2()
+        {
+            CreateMap<Size, SizeViewModel>(
+                s => new SizeViewModel()
+                {
+                    Width = s.Width.ToString() + "m",
+                    Height = s.Height.ToString() + "m",
+                    Depth = s.Depth.ToString() + "m"
+                });
+        }
+    }
+}
---
+++ src/ExplicitMapper/ExplicitMapper.Tests/Integration/SeveralMappingConfigurationClasses/SeveralMappingConfigurationClassesTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ExplicitMapper.Tests.Integration.SeveralMappingConfigurationClasses
+{
+    [Collection("Integration tests")]
+    [Trait("Integration", "Several mapping confguration classes")]
+    public class SeveralMappingConfigurationClassesTests : IDisposable
+    {
+        [Fact(DisplayName = "Use three mapping configuration classes")]
+        public void SeveralMappingConfigurationClasses_FieldsMapped()
+        {
+            MappingConfiguration.Add<Configuration1>();
+            MappingConfiguration.Add<Configuration2>();
+            MappingConfiguration.Add<Configuration3>();
+            MappingConfiguration.Build();
+
+            var product = new Product()
+            {
+                Manufacturer = "Company",
+                Name = "Good product",
+                Size = new Size()
+                {
+                    Width = 1,
+                    Height = 2,
+                    Depth = 3
+                }
+            };
+
+            var productViewModel = Mapper.Map<ProductViewModel>(product);
+
+            productViewModel.Should().NotBeNull();
+            productViewModel.Description.Should().Be("Company - Good product");
+            productViewModel.Size.Should().NotBeNull();
+            productViewModel.Size.Width.Should().Be("1m");
+            productViewModel.Size.Height.Should().Be("2m");
+            productViewModel.Size.Depth.Should().Be("3m");
+
+            var x = new X()
+            {
+                X1 = 1,
+                X2 = 2
+            };
+
+            var y = Mapper.Map<Y>(x);
+
+            y.Should().NotBeNull();
+            y.Y1.Should().Be(x.X1);
+            y.Y2.Should().Be(x.X2);
+        }
+
+        public void Dispose()
+        {
+            MappingConfiguration.Clear();
+        }
+    }
+}
